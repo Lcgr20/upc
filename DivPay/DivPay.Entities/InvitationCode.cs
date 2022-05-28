411d43c0098060507415ae5057ca5bdcef7bf866
RenzoPV <67492305+RenzoPV@users.noreply.github.com>
2022-05-27 19:14:25 -0500

    divpay-api

---
+++ DivPay/DivPay.Entities/InvitationCode.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DivPay.Entities;
+
+[Table("CodigosdeInvitacion")]
+public class InvitationCode: EntityBase
+{
+    public int NumInvitados { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; }
+}
+
