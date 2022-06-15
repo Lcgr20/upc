c17488d80ef0d705fffaeeb1bf98b126bb80cd76
RenzoPV <67492305+RenzoPV@users.noreply.github.com>
2022-06-15 18:59:00 -0500

    centro de ayuda añadido

--- Frontend/src/app/components/centro-ayuda/centro-ayuda.component.ts
+++ Frontend/src/app/components/centro-ayuda/centro-ayuda.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentroAyudaComponent implements OnInit {
 
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
+
+
